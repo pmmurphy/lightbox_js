3d128c1a07abe3dd8f21c5b6460aabfac27a5cb6
Paula Murphy <paulam849@gmail.com>
2015-12-11 14:54:10 -0500

    add lightbox

--- scriptlb.js
+++ scriptlb.js
@@ -1,9 +1,24 @@
 $(document).ready(
 	function(){
 
-	$(".light_link").on("click", function(){
-	console.log("you have clicked");
+	$(".lightbox_link").on("click",function(e){
+		e.preventDefault();
 	
-	})
+	
+		$(".outer_lightbox").show();
+		$(".outer_lightbox").on("click",function(){
+			$(".outer_lightbox").hide();
 
-})
+
+			// $(".inner_lightbox").show();
+			// $(".inner_lightbox").on("click",function(){
+			// $(".outer_lightbox").hide();
+
+		});
+	});
+
+});
+
+// (e).preventDefault dummy variable preventing the default of the anchor tag the hastag is gone. Holding off overriding the default. 
+
+// (this) means click on one event on the anchor tags. This refers to the thing you click on. it knows what you are click on. 
